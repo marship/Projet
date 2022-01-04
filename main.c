73b2bb43ed1ef5445b67ab2826345e9720d3bdef
dcs-b <81160533+dcs-b@users.noreply.github.com>
2022-01-04 23:51:43 +0100

    Modification de la gestion des options

--- main.c
+++ main.c
@@ -1,47 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <getopt.h>
 #include "lecture_entete.h"
 #include "elf_header.h"
 
 
-int main(int argc, char **argv) {
+void usage(char *name) {
+	fprintf(stderr,
+        "Usage:\n"
+        "  %s <options> fichiers-elf\n"
+        "\n"
+        "Options:\n"
+        "  -h --file-header       Afficher l'en-tête du fichier ELF\n"
+        "  -H --help              Afficher l'aide-mémoire\n"
+		, name);
+}
 
-    if (argc < 3) {
-        fprintf(stderr, "Nombre d'arguments incorrect\n");
+int main(int argc, char **argv) {
+	int opt;
+	char *nom_fichier = NULL;
+
+    if (argc < 2) {
+        usage(argv[0]);
         return EXIT_FAILURE;
     }
 
-    char c = argv[1][0]; 
+	struct option longopts[] = {
+		{ "file-header", required_argument, NULL, 'h' },
+		{ "help", no_argument, NULL, 'H' },
+		{ NULL, 0, NULL, 0 }
+	};
 
-    if (c == 'a'){
-        printf("\n\t\t\t-----MODE EMPLOI-----\n\n\th : Affichage de l'en-tete\n\tS : Affichage de la table des sections\n\tx : Affichage du contenu d'une section\n\ts : Affichage de la table des symboles\n\tr : Affichage des tables de reimplantation\n");
-    }
-    switch (c)
-    {
-    case 'h':
-        lire_entete(argv[2]);
-        break;
+	while ((opt = getopt_long(argc, argv, "h:H", longopts, NULL)) != -1) {
+		switch(opt) {
+		case 'h':
+			nom_fichier = optarg;
+            lire_entete(nom_fichier);
+			break;
+		case 'H':
+			usage(argv[0]);
+			return EXIT_SUCCESS;
+		default:
+			usage(argv[0]);
+			return EXIT_FAILURE;
+		}
+	}
 
-    case 'S':
-        printf("TO DO");
-        break;
-    
-    case 'x':
-        printf("TO DO");
-        break;
-    
-    case 's':
-        printf("TO DO");
-        break;
-    
-    case 'r':
-        printf("TO DO");
-        break;
-    
-    default:
-        break;
-    }
+    //     printf("\n\t\t\t-----MODE EMPLOI-----\n\n\th : Affichage de l'en-tete\n\tS : Affichage de la table des sections\n\tx : Affichage du contenu d'une section\n\ts : Affichage de la table des symboles\n\tr : Affichage des tables de reimplantation\n");
 
     return EXIT_SUCCESS;
 }
