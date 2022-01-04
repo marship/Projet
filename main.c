38792444a6f4e8e4ab9583baaee3df722ac9784e
marship <61384536+marship@users.noreply.github.com>
2022-01-04 18:40:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,46 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "lecture_entete.h"
-#include "entete_elf.h"
+#include "elf_header.h"
 
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
+
+    if (argc < 3) {
         fprintf(stderr, "Nombre d'arguments incorrect\n");
         return EXIT_FAILURE;
     }
 
-    lire_entete(argv[1]);
+    char c = argv[1][0]; 
+
+    if (c == 'a'){
+        printf("\n\t\t\t-----MODE EMPLOI-----\n\n\th : Affichage de l'en-tete\n\tS : Affichage de la table des sections\n\tx : Affichage du contenu d'une section\n\ts : Affichage de la table des symboles\n\tr : Affichage des tables de reimplantation\n");
+    }
+    switch (c)
+    {
+    case 'h':
+        lire_entete(argv[2]);
+        break;
+
+    case 'S':
+        printf("TO DO");
+        break;
+    
+    case 'x':
+        printf("TO DO");
+        break;
+    
+    case 's':
+        printf("TO DO");
+        break;
+    
+    case 'r':
+        printf("TO DO");
+        break;
+    
+    default:
+        break;
+    }
 
     return EXIT_SUCCESS;
 }
