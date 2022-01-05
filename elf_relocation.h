312eabbc926a733a35218f43f34f740020df6a02
Paul Faure-Jeunot <61384536+marship@users.noreply.github.com>
2022-01-05 08:44:56 +0100

    Add files via upload

---
+++ elf_relocation.h
@@ -0,0 +1,23 @@
+#ifndef __ELF_RELOCATION_H__
+#define __ELF_RELOCATION_H__
+
+#include "elf_types.h"
+
+/* ELF32 Structure de la table des symboles en 32-bit */
+typedef struct {
+    Elf32_Addr r_offset;
+    Elf32_Word r_info;
+} Elf32_Rel;
+
+typedef struct {
+    Elf32_Addr r_offset;
+    Elf32_Word r_info;
+    Elf32_Sword r_addend;
+} Elf32_Rela;
+
+/* r_info */
+#define ELF32_R_SYM(i) ((i)>>8)
+#define ELF32_R_TYPE(i) ((unsigned char)(i))
+#define ELF32_R_INFO(s,t) (((s)<<8)+(unsigned char)(t))
+
+#endif
