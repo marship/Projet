0324933514b3f5fbbf24e050e6273ca3d728c3d7
Paul Faure-Jeunot <61384536+marship@users.noreply.github.com>
2022-01-05 21:41:59 +0100

    Update lecture_symbole.h

--- lecture_symbole.h
+++ lecture_symbole.h
@@ -1,12 +1,18 @@
 #ifndef __LECTURE_SYMBOLE_H__
 #define __LECTURE_SYMBOLE_H__
 
+#include "elf_relocation.h"
+#include "elf_header.h"
+#include "section_header.h"
+#include "elf_types.h"
+#include "elf_symbol.h"
+
 /**
  * @brief 
  * 
  * TO DO Description
  * 
  */
-void lire_symbole();
+void *lire_symbole(char *nom_fichier, Elf32_Ehdr ehdr, Elf32_Shdr *shdr);
 
 #endif
