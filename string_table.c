8277620054ba02cd2e7d37f9a80503ff432ecc2f
dcs-b <81160533+dcs-b@users.noreply.github.com>
2022-01-06 22:41:05 +0100

    Modification string_table La fonction lire_string_table gère désormais l'ouverture et la fermeture du fichier

--- string_table.c
+++ string_table.c
@@ -4,7 +4,14 @@
 #include "string_table.h"
 
 
-char *lire_string_table(Elf32_Addr addr, Elf32_Word size, FILE *f, char *nom_fichier) {
+char *lire_string_table(Elf32_Addr addr, Elf32_Word size, char *nom_fichier) {
+    FILE *f = fopen(nom_fichier, "rb");
+
+    if (f == NULL) {
+        fprintf(stderr, "Error: Can't open file %s\n", nom_fichier);
+        exit(EXIT_FAILURE);
+    }
+
     if (fseek(f, addr, SEEK_SET) != 0) {
         fprintf(stderr, "Erreur de lecture du fichier %s\n", nom_fichier);
         exit(EXIT_FAILURE);
@@ -19,23 +26,24 @@ char *lire_string_table(Elf32_Addr addr, Elf32_Word size, FILE *f, char *nom_fic
         }
     }
 
+    fclose(f);
     return strtab;
 }
 
 
-char *lire_strtab(Elf32_Shdr *shdr, Elf32_Ehdr ehdr, FILE *f, char *nom_fichier) {
+char *lire_strtab(Elf32_Shdr *shdr, Elf32_Ehdr ehdr, char *nom_fichier) {
     for (Elf32_Half i = 0; i < ehdr.e_shnum; i++) {
         if (shdr[i].sh_type == SHT_STRTAB && i != ehdr.e_shstrndx) {
-            return lire_string_table(shdr[i].sh_addr + shdr[i].sh_offset, shdr[i].sh_size, f, nom_fichier);
+            return lire_string_table(shdr[i].sh_addr + shdr[i].sh_offset, shdr[i].sh_size, nom_fichier);
         }
     }
     fprintf(stderr, "Erreur: string table absente\n");
     exit(EXIT_FAILURE);
 }
 
-char *lire_shstrtab(Elf32_Shdr *shdr, Elf32_Ehdr ehdr, FILE *f, char *nom_fichier) {
+char *lire_shstrtab(Elf32_Shdr *shdr, Elf32_Ehdr ehdr, char *nom_fichier) {
     int i = ehdr.e_shstrndx;
-    return lire_string_table(shdr[i].sh_addr + shdr[i].sh_offset, shdr[i].sh_size, f, nom_fichier);
+    return lire_string_table(shdr[i].sh_addr + shdr[i].sh_offset, shdr[i].sh_size, nom_fichier);
 }
 
 void afficher_chaine(char *strtab, Elf32_Word index) {
--- string_table.h
+++ string_table.h
@@ -4,9 +4,9 @@
 #include "elf_header.h"
 #include "section_header.h"
 
-char *lire_strtab(Elf32_Shdr *shdr, Elf32_Ehdr ehdr, FILE *f, char *nom_fichier);
+char *lire_strtab(Elf32_Shdr *shdr, Elf32_Ehdr ehdr, char *nom_fichier);
 
-char *lire_shstrtab(Elf32_Shdr *shdr, Elf32_Ehdr ehdr, FILE *f, char *nom_fichier);
+char *lire_shstrtab(Elf32_Shdr *shdr, Elf32_Ehdr ehdr, char *nom_fichier);
 
 void afficher_chaine(char *strtab, Elf32_Word index);
 
