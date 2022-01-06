3b109f880f3f6ec1f38a77617e7d531b8eb5cb87
Aglemba <94596458+Aglemba@users.noreply.github.com>
2022-01-06 10:08:13 +0100

    Modification lecture_symbole.h
    Ajout de signature de fonction

--- lecture_symbole.h
+++ lecture_symbole.h
@@ -8,19 +8,25 @@
 #include "elf_symbol.h"
 
 /**
- * @brief 
- * 
- * TO DO Description
- * 
+ * @brief Fonction qui crée une structure contenant la table des symboles
+ *
+ * @param nom_fichier (char *) Nom du fichier ELF
+ * @param ehdr (Elf32_Ehdr *) Structure contenant l'entête pour obtenir le type de l'endian
+ * @param shdr (Elf32_Shdr *) Structure contenant l'adresse pour le début de la section
+ * @return (Elf32_Sym *) Renvoie une Structure correspondant à la table des symboles
  */
 Elf32_Sym *lire_symbole(char *nom_fichier, Elf32_Ehdr ehdr, Elf32_Shdr *shdr);
 
 /**
- * @brief 
- * 
- * TO DO Description
- * 
+ * @brief Fonction qui affiche la table des symboles corresondant à un fichier ELF
+ *
+ * @param sym (Elf32_Sym *) Structure contenant les symboles
+ * @param nom_fichier (char *) Nom du fichier ELF
  */
+<<<<<<< Updated upstream
 void afficher_symboles(Elf32_Sym *sym, Elf32_Ehdr ehdr, Elf32_Shdr *shdr, char *nom_fichier);
+=======
+void afficher_symboles(Elf32_Sym *sym, char *nom_fichier);
+>>>>>>> Stashed changes
 
 #endif
