420d3dc67c60c6729165f3a7c21a6add147a71db
dcs-b <81160533+dcs-b@users.noreply.github.com>
2022-01-06 17:00:24 +0100

    Ajout option relocs dans main

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "elf_header.h"
 #include "fonctions_utilitaires.h"
 #include "lecture_symbole.h"
+#include "lecture_reimplantation.h"
 
 
 void usage(char *name)
@@ -17,6 +18,7 @@ void usage(char *name)
             "Options:\n"
             "  -h --file-header       Afficher l'en-tête du fichier ELF\n"
             "  -s --table-symbole     Afficher la table des symboles\n"
+			"  -r --relocs            Afficher les réadressages (si présents)\n"
             "  -H --help              Afficher l'aide-mémoire\n",
             name);
 }
@@ -26,7 +28,6 @@ int main(int argc, char **argv)
 
 	int opt;
 	char *nom_fichier = NULL;
-	Elf32_Ehdr ehdr;
 
     if (argc < 2)
     {
@@ -37,21 +38,26 @@ int main(int argc, char **argv)
 	struct option longopts[] = {
 		{ "file-header", required_argument, NULL, 'h' },
         { "table-symbole", required_argument, NULL, 's' },
+		{ "relocs", required_argument, NULL, 'r'},
 		{ "help", no_argument, NULL, 'H' },
 		{ NULL, 0, NULL, 0 }
 	};
+
+	Elf32_Ehdr ehdr = lire_entete(nom_fichier);
 
 	while ((opt = getopt_long(argc, argv, "h:s:H", longopts, NULL)) != -1) {
 		switch(opt) {
 		case 'h':
 			nom_fichier = optarg;
-            ehdr = lire_entete(nom_fichier);
 			afficher_entete(ehdr);
 			break;
         case 's':
 			nom_fichier = optarg;
             lire_symbole(nom_fichier);
             break;
+		case 'r':
+
+			break;
 		case 'H':
 			usage(argv[0]);
 			return EXIT_SUCCESS;
