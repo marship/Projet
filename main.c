57334bd6ce076889d709df6d498efa1ec374d367
Paul Faure-Jeunot <61384536+marship@users.noreply.github.com>
2022-01-06 16:57:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "elf_header.h"
 #include "fonctions_utilitaires.h"
 #include "lecture_symbole.h"
+#include "lecture_section.h"
 
 
 void usage(char *name)
@@ -16,6 +17,7 @@ void usage(char *name)
             "\n"
             "Options:\n"
             "  -h --file-header       Afficher l'en-tête du fichier ELF\n"
+			"  -S --section-header    Afficher l'en-tête des sections\n"
             "  -s --table-symbole     Afficher la table des symboles\n"
             "  -H --help              Afficher l'aide-mémoire\n",
             name);
@@ -27,6 +29,7 @@ int main(int argc, char **argv)
 	int opt;
 	char *nom_fichier = NULL;
 	Elf32_Ehdr ehdr;
+	Elf32_Shdr *shdr;
 
     if (argc < 2)
     {
@@ -36,21 +39,30 @@ int main(int argc, char **argv)
 
 	struct option longopts[] = {
 		{ "file-header", required_argument, NULL, 'h' },
-        { "table-symbole", required_argument, NULL, 's' },
+        { "section-header ", required_argument, NULL, 'S' },
+		{ "table-symbole", required_argument, NULL, 's' },
 		{ "help", no_argument, NULL, 'H' },
 		{ NULL, 0, NULL, 0 }
 	};
 
-	while ((opt = getopt_long(argc, argv, "h:s:H", longopts, NULL)) != -1) {
+	while ((opt = getopt_long(argc, argv, "h:S:s:H", longopts, NULL)) != -1) {
 		switch(opt) {
 		case 'h':
 			nom_fichier = optarg;
             ehdr = lire_entete(nom_fichier);
 			afficher_entete(ehdr);
 			break;
+
+		case 'S':
+			nom_fichier = optarg;
+            ehdr = lire_entete(nom_fichier);
+			shdr = lire_section(nom_fichier, ehdr);
+			afficher_section(shdr, ehdr);
+			break;
         case 's':
 			nom_fichier = optarg;
-            lire_symbole(nom_fichier);
+			printf("wesh alors");
+            //lire_symbole(nom_fichier);
             break;
 		case 'H':
 			usage(argv[0]);
@@ -60,8 +72,5 @@ int main(int argc, char **argv)
 			return EXIT_FAILURE;
 		}
 	}
-
-    //     printf("\n\t\t\t-----MODE EMPLOI-----\n\n\th : Affichage de l'en-tete\n\tS : Affichage de la table des sections\n\tx : Affichage du contenu d'une section\n\ts : Affichage de la table des symboles\n\tr : Affichage des tables de reimplantation\n");
-
     return EXIT_SUCCESS;
 }
