b870bc000523844857fae0d4120844bfad3466d0
Paul Faure-Jeunot <61384536+marship@users.noreply.github.com>
2022-01-11 11:08:47 +0100

    Update section_sans_rel.h

--- section_sans_rel.h
+++ section_sans_rel.h
@@ -10,5 +10,6 @@
 uint32_t nb_rel(Elf32_Ehdr ehdr, Elf32_Shdr *shdr);
 Elf32_Shdr *maj_section(Elf32_Ehdr ehdr, Elf32_Shdr *shdr);
 uint32_t index_string(Elf32_Ehdr ehdr, Elf32_Shdr *shdr);
+Elf32_Sym *maj_ndx(Elf32_Sym *sym, Elf32_Shdr *shdr, Elf32_Ehdr ehdr);
 
 #endif
